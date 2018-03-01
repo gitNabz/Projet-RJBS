aa0e1959d86e23c9d075a9b20e44a7d763a2c1e3
asmailyes <chibane.samira41@gmail.com>
2018-03-01 16:49:19 +0100

    maj

---
+++ models/Menu.class.php
@@ -0,0 +1,51 @@
+<?php
+class Menu
+{
+	private $id;
+	private $name;
+	private $description;
+	private $price;
+	private $id_fnb
+
+	public function getId()
+	{
+		return $this->id;
+	}
+	public function getName()
+	{
+		return $this->name;
+	}
+	public function setName($Name)
+	{
+		$this->name = $name;
+	}
+	public function getDescription()
+	{
+		return $this->description;
+	}
+	public function setDescription($description)
+	{
+		$this->description = $description;
+	}
+	
+	public function getPrice()
+	{
+		return $this->price;
+	}
+	public function setPrice($Price)
+	{
+		$this->price = $price;
+	}
+	public function getId_fnb()
+	{
+		return $this->id_fnb;
+	}
+	public function setId_fnb($Id_fnb)
+	{
+		$this->id_fnb->$id_fnb;
+	}
+
+
+
+}
+?>
