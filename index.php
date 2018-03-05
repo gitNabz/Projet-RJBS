2c6b489c05b960cc25e899715a529f4e1005ce5e
gitRV <troisthreetres@yopmail.com>
2018-03-05 15:23:03 +0100

    updating3

--- index.php
+++ index.php
@@ -19,12 +19,8 @@ try
 	$error='';
 	$page = 'home';
 
-<<<<<<< HEAD
-	$access = ['home', 'menu', 'edit','admin', 'booking', 'delivery', 'createfnb', 'carte'];
 
-=======
-	$access = ['home', 'menu', 'edit','admin', 'booking','createfnb','carte', 'delivery'];
->>>>>>> 037150262788e850c0a0931fe92cb123e95b596e
+	$access = ['home', 'menu', 'edit','admin', 'booking', 'delivery', 'createfnb', 'carte'];
 
 	
 	if (isset($_GET['page']))
